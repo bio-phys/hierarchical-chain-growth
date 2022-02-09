63e0927c3d4f6b1ae12e8cb08c77de6f90cd6ef8
LisaPietrek <lisapietrek@gmail.com>
2022-02-09 16:08:14 +0100

    update functions

--- rhcg/tauK18/lib.py
+++ rhcg/tauK18/lib.py
@@ -1,6 +1,5 @@
 import MDAnalysis as mda
 import numpy as np
-from MDAnalysis.analysis import align
 from MDAnalysis.analysis.rms import rmsd
 import MDAnalysis.analysis.distances as distances
 
@@ -25,20 +24,67 @@ def translate_concept(u1, u2,index1_alnB=-3, index1_alnE=-2, index2_alnB=1, inde
     return select
 
 
-# In[54]:
+def findClashes(u1,u2, index1b=-3, index2=3, 
+                 index1e=-1, clash_radius=2.0):
+    """ finds clashes: atoms with a distance below clash_radius
+    
+    Parameters
+    ----------
+    u1 : universe
+        fragment 1 to pair
+    u2 : universe
+        fragment 2 to pair
+    index1b : integer
+        residue index for residues to *exclude* from clash detection
+        marks residue to begin exclusion for u1
+    index2e : integer
+        residue index for residues to *exclude* from clash detection
+        marks residue to end exclusion for u2
+    index1e : integer
+        residue index for residues to *exclude* from clash detection
+        marks residue to end exclusion for u1. The default is -1
+    clash_distance : float
+        max. allowed distance between atoms, everything below is counted as clash
+        The  default is 2.0
 
-def findClashes(u1,u2, index1b=-2, index1e=-1, index2=3, clash_radius=2.7):
-    # atom selection of first fragment
-    l1 = u1.select_atoms("protein and not (type H) and not resid {}:{}".format(u1.atoms.residues[index1b].resid , u1.atoms.residues[index1e].resid)) 
-    # atom selection of subsequent fragmenmt
-    l2 = u2.select_atoms("protein and not (type H) and not resid 1:{}".format(u2.atoms.residues[index2].resid))
+    Returns
+    -------
+    clsum : integer
+        count of clashes between aligned fragments
+        
+    NOTE: MDAnalysis is inclusive! resid 1:2 -> selects residues 1+2
+    """
+    l1 = u1.select_atoms("protein and not (type H) and not (resid {} and backbone) and not resid {}:{}".format(
+                                                    u1.atoms.residues[index1b].resid,
+                                                    u1.atoms.residues[index1b+1].resid,
+                                                    u1.atoms.residues[index1e].resid))
 
+    # atom selection of u2 to scan for clashes
+    l2 = u2.select_atoms("protein and not (type H) and not resid 1:{} and not (resid {} and backbone)".format(
+                                                    u2.atoms.residues[index2-1].resid,
+                                                    u2.atoms.residues[index2].resid))
+    # (problem with finding correct python c++ libraries for KD neighbour search)
+    # -> use mda.distances to generate a matrix with distances
     ## code snippet from Soeren von Buelow
     distmat = distances.distance_array(l1.positions,l2.positions)
     cont = np.less(distmat,clash_radius) # see where distmat < cutoff
-    cont = np.where(cont,1,0) # True --> 1, False --> 0 (to count pairs)
+    cont = np.where(cont,1,0) # True --> 1, False --> 0
     clsum = np.sum(cont)
+    
     return clsum
+
+#def findClashes(u1,u2, index1b=-2, index1e=-1, index2=3, clash_radius=2.7):
+#    # atom selection of first fragment
+#    l1 = u1.select_atoms("protein and not (type H) and not resid {}:{}".format(u1.atoms.residues[index1b].resid , u1.atoms.residues[index1e].resid)) 
+#    # atom selection of subsequent fragmenmt
+#    l2 = u2.select_atoms("protein and not (type H) and not resid 1:{}".format(u2.atoms.residues[index2].resid))
+#
+#    ## code snippet from Soeren von Buelow
+#    distmat = distances.distance_array(l1.positions,l2.positions)
+#    cont = np.less(distmat,clash_radius) # see where distmat < cutoff
+#    cont = np.where(cont,1,0) # True --> 1, False --> 0 (to count pairs)
+#    clsum = np.sum(cont)
+#    return clsum
 
 
 
