239cbf22c850077539051352df672b499e875e57
LisaPietrek <lisapietrek@gmail.com>
2022-02-15 11:56:40 +0100

    rename folder

---
+++ chain_growth/check_sequence.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from fragmentList import get_sequence_from_fasta, get_sequence_from_pdb
+import os #, pytest 
+
+# path to topology file of idp grown with (r)hcg
+file_hcg = 'example/test/4/0/pair0.pdb'
+sequence_hcg = get_sequence_from_pdb(file_hcg)
+
+# path to reference sequence == inout sequence
+file_ref =  'example/short_tau.fasta'
+## get amino acid sequence from PDB or fasta file in three letter code
+file_type = os.path.basename(file_ref).split('.')[-1]
+if file_type.upper() == 'FASTA':
+    sequence_ref = get_sequence_from_fasta(file_ref)
+elif file_type.upper() == 'PDB':
+    sequence_ref = get_sequence_from_pdb(file_ref)
+    
+#print(sequence_hcg, sequence_ref)
+assert (sequence_hcg == sequence_ref)
