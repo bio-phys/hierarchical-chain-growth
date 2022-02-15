4a0b77fdd43b528b8febabc896fa99a385ec5dfa
LisaPietrek <lisapietrek@gmail.com>
2022-02-15 14:32:35 +0100

    run (r)hcg - docstring

--- chain_growth/run_rhcg.py
+++ chain_growth/run_rhcg.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-
+"""
+run_rhcg
+--------
+run reweighted hierarchical chain growth
+"""
 from hcgList import make_hcl_l
 from fragmentList import generate_fragment_list
 from hcgGrow_fct import reweighted_hierarchical_chain_growth
@@ -10,9 +14,9 @@ from hcgGrow_fct import reweighted_hierarchical_chain_growth
 ################
 
 ## input file and path / output path
-## path to MD fragments
+# path to MD fragments
 path0 = 'example/'
-## path to store assembled models in
+# path to store assembled models in
 path = 'example/test_weighted/'
 #path weights
 path2weights = '/home/lisa/PhD/projects/tau/analysis/HCG/BioEnImportanceSampling/cs/f2_rc'
@@ -20,6 +24,7 @@ path2weights = '/home/lisa/PhD/projects/tau/analysis/HCG/BioEnImportanceSampling
 sequence_f = 'example/short_tau.fasta'
 # theta for fragment reweighting
 theta=10.0
+
 ## define fragment construction
 # length of MD fragments (without the end-capping groups if present)
 fragment_length = 5
@@ -35,8 +40,8 @@ fragment_l, overlaps_d = generate_fragment_list(sequence_f, fragment_length, ove
 ## lists for the HCG
 n_pairs = len(fragment_l)
 print(fragment_l, overlaps_d, n_pairs)
-## hcg_l : list of paired fragments
-## promo_l : list to evaluate if last fragment of level m in hcg_l is promoted to level m+1
+# hcg_l : list of paired fragments
+# promo_l : list to evaluate if last fragment of level m in hcg_l is promoted to level m+1
 hcg_l, promo_l = make_hcl_l(n_pairs)
 print(hcg_l)
 
@@ -52,15 +57,3 @@ reweighted_hierarchical_chain_growth(hcg_l, promo_l, overlaps_d, path0, path, km
         capping_groups=capping_groups,  path2weights=path2weights, theta=theta)#, verbose=True) 
     
 
-#############
-## run test -compare output development - flexible version for short tau K18
-#############
-#
-# index list
-# import pickle
-# rp = pickle.load(open( "{}/confIndex_Old_all.pkl".format(path), "rb" ) )
-# ###########
-# ## run HCG
-# ###########
-# reweighted_hierarchical_chain_growth(hcg_l, promo_l, overlaps_d, path0, path, kmax=kmax, ri_l=rp,
-#         capping_groups=capping_groups,  path2weights=path2weights, theta=theta)#, verbose=True) 
