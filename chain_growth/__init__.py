77f8d19e43bf39fe5234abf8f796a5ba368f7954
LisaPietrek <lisapietrek@gmail.com>
2022-02-16 18:07:28 +0100

    update __init__.py

--- chain_growth/__init__.py
+++ chain_growth/__init__.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from .hcg_fct import hierarchical_chain_growth, reweighted_hierarchical_chain_growth
+from .fragment_list import get_sequence_from_pdb, get_sequence_from_fasta, generate_fragment_list
