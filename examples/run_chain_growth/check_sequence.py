19f00fb8be1a3a1c53d5a6a493e3b63b419d2917
LisaPietrek <lisapietrek@gmail.com>
2022-02-16 18:42:09 +0100

    correct path

--- examples/run_chain_growth/check_sequence.py
+++ examples/run_chain_growth/check_sequence.py
@@ -11,11 +11,11 @@ from fragmentList import get_sequence_from_fasta, get_sequence_from_pdb
 import os #, pytest 
 
 # path to topology file of idp grown with (r)hcg
-file_hcg = 'example/test/4/0/pair0.pdb'
+file_hcg = 'truncated_tauK18/4/0/pair0.pdb'
 sequence_hcg = get_sequence_from_pdb(file_hcg)
 
 # path to reference sequence == inout sequence
-file_ref =  'example/short_tau.fasta'
+file_ref =  '../truncated_tauK18.fasta'
 ## get amino acid sequence from PDB or fasta file in three letter code
 file_type = os.path.basename(file_ref).split('.')[-1]
 if file_type.upper() == 'FASTA':
