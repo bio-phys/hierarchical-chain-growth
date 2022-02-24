109fee484ae1639a9dbe2d5a092445b9b2b08d98
LisaPietrek <lisapietrek@gmail.com>
2022-02-24 09:25:41 +0100

    fix test

--- tests/integration/test_hcg.py
+++ tests/integration/test_hcg.py
@@ -4,7 +4,7 @@
 from chain_growth.fragment_list import get_sequence_from_fasta, get_sequence_from_pdb
 import os , pytest 
 
-def check_sequence():#path_hcg_fulllength, path_ref):
+def test_sequence():#path_hcg_fulllength, path_ref):
     """
     check_sequence
     --------------
@@ -13,11 +13,11 @@ def check_sequence():#path_hcg_fulllength, path_ref):
     """
     
     # path to topology file of idp grown with (r)hcg
-    file_hcg = '../../examples/run_chain_growth/truncated_tauK18/4/0/pair0.pdb'
+    file_hcg = 'examples/run_chain_growth/truncated_tauK18/4/0/pair0.pdb'
     sequence_hcg = get_sequence_from_pdb(file_hcg)
     
     # path to reference sequence == inout sequence
-    file_ref =  '../../examples/truncated_tauK18.fasta'
+    file_ref =  'examples/truncated_tauK18.fasta'
     ## get amino acid sequence from PDB or fasta file in three letter code
     file_type = os.path.basename(file_ref).split('.')[-1]
     if file_type.upper() == 'FASTA':
